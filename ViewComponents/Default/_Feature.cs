58f5f1354138ef3981c898c370c306f3be4ec7d4
meltemSavas <you@example.com>
2023-07-13 12:22:27 +0300

    Add project files.

---
+++ ViewComponents/Default/_Feature.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _Feature : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+           
+            return View();
+          
+        }
+    }
+}
---
+++ ViewComponents/Default/_SubAbout.cs
@@ -0,0 +1,20 @@
+﻿using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _SubAbout : ViewComponent
+    {
+        SubAboutManager subAboutManager = new BusinessLayer.Concrete.SubAboutManager(new EfSubAboutDal());
+        public IViewComponentResult Invoke()
+        {
+            var values= subAboutManager.TGetList(); 
+           
+            return View(values);
+
+        }
+
+    }
+}

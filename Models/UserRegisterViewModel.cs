416e071907fa8e558fc3766bcbee4fcf98d30615
meltemSavas <you@example.com>
2023-07-17 16:32:03 +0300

    authorize

---
+++ Models/UserRegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProje.Models
+{
+	public class UserRegisterViewModel
+	{
+		[Required(ErrorMessage = "Lütfen Adınızı Giriniz")]
+        public string Name { get; set; }
+
+		[Required(ErrorMessage = "Lütfen Soyadınızı Giriniz")]
+		public string Surname { get; set; }
+		[Required(ErrorMessage = "Lütfen Kullanıcı Adınızı Giriniz")]
+		public string UserName { get; set; }
+
+		[Required(ErrorMessage = "Lütfen Mail Adresinizi Giriniz")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Lütfen Şifrenizi Giriniz")]
+		public string Password { get; set; }
+
+		[Required(ErrorMessage = "Lütfen Şifrenizi Tekrar Giriniz")]
+		[Compare("Password", ErrorMessage="Şifreler Uyumlu Değil")]
+		public string ConfirmPassword { get; set; }
+	}
+}

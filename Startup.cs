a1b0235574cc68e520cf968ef51edd28cb11ea35
meltemSavas <you@example.com>
2023-07-18 12:52:01 +0300

    userLayout Partiallara ayrıldı

--- Startup.cs
+++ Startup.cs
@@ -68,6 +68,14 @@ namespace TraversalCoreProje
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
         }
     }
 }

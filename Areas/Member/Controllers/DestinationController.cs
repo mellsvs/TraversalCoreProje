25c2991c2466caf6fea7c2fb950d928ca8cbf33e
meltemSavas <you@example.com>
2023-07-18 16:56:59 +0300

    signin

---
+++ Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ Models/UserSignInViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProje.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage = "Lütfen Adınızı Giriniz")]
+        public string username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi Giriniz")]
+        public string password { get; set; }
+    }
+}
